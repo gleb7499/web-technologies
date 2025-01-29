1da442230e7c5b3c95ea022fd29933d7f1e6ad41
gleb7499 <164905074+gleb7499@users.noreply.github.com>
2025-01-29 22:58:12 +0300

    Lab_4: Добавлена работа 4

---
+++ subgroup1/Loginov_Gleb/Lab_4/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <SearchFlights />
+  </div>
+</template>
+
+<script>
+import SearchFlights from './components/SearchFlights.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SearchFlights
+  }
+}
+</script>
+
+<style>
+
+</style>
